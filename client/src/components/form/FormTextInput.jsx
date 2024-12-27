34012790c9c0e966556e9a79b83cd5e9864e6d39
athulkrishnama <athulkrishnama24@gmail.com>
2024-12-27 10:45:00 +0530

    component for form input field

---
+++ client/src/components/form/FormTextInput.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Field } from "formik";
+import { motion, AnimatePresence } from "framer-motion";
+function FormTextInput({ touched, error, label, name, classes, ...props }) {
+  return (
+    <div className="flex flex-col  min-h-20">
+      <label htmlFor={name}>{label}</label>
+      <Field name={name} {...props} className="border border-gray-400 rounded-md px-2 py-1"/>
+      <AnimatePresence>
+        {touched && error && <AnimatedErrorMessage classess={classes}>{error}</AnimatedErrorMessage>}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+function AnimatedErrorMessage({ children , classess} ) {
+  const errorClass = "text-red-600 text-wrap " + classess;
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -10 }}
+      className={errorClass}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default FormTextInput;
