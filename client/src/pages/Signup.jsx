e9efd883c862cc2ab5501659ffe23f44cba01b06
athulkrishnama <athulkrishnama24@gmail.com>
2024-12-28 12:05:00 +0530

    sign page that will show signup form and otp page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,34 @@
-import SignupForm from "../components/signup/Signup"
+import SignupForm from "../components/signup/Signup";
+import OtpPage from "../components/signup/Otp";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import logo from "../assets/timelens.png";
 function Signup() {
+  const [isOtpSent, setOtpSent] = useState(false);
   return (
-    <SignupForm/>
-  )
+    <div className="h-screen flex flex-col bg-slate-100 gap-3 ">
+      <motion.nav
+        className="px-10  py-1 shadow-2xl rounded-3xl bg-white"
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <motion.img src={logo} className="h-20" alt="logo" />
+      </motion.nav>
+      <AnimatePresence>
+        {!isOtpSent ? (
+          <motion.div
+          className="flex-grow">
+            <SignupForm setOtpSent={setOtpSent} />
+          </motion.div>
+        ) : (
+          <motion.div
+          className="flex-grow">
+            <OtpPage />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
