bc377032d280e98416f1d58dc29f399d97404747
athulkrishnama <athulkrishnama24@gmail.com>
2024-12-29 18:33:28 +0530

    wrap App component with Redux Provider for state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
+import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router'
+import { Provider } from 'react-redux';
+import store from './store/store';
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
 import {ToastContainer,} from 'react-toastify'
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>} />
-        </Routes>
-      </BrowserRouter>
-      <ToastContainer/>
-    </>
-  )
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/login" element={<Login/>} />
+                    <Route path="/signup" element={<Signup/>} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer/>
+        </Provider>
+    )
 }
 
 export default App
