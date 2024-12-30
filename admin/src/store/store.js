25a884228c6ca11465c042754be588f3bfdd7031
athulkrishnama <athulkrishnama24@gmail.com>
2024-12-30 17:02:16 +0530

    redux store created

---
+++ admin/src/store/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './userSlice/userSlice.js';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user.accessToken'],
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+const store = configureStore({
+  reducer: {
+    user: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
