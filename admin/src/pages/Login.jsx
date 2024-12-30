f02ced16792aa0b3666a79202955ebd3cfcce664
athulkrishnama <athulkrishnama24@gmail.com>
2024-12-30 17:02:30 +0530

    pages for login and home created

---
+++ admin/src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import Header from "../components/login/Header";
+import ProtectedLogin from "../components/protectedComponents/ProtectedLogin";
+import LoginForm from "../components/login/LoginForm";
+function Login() {
+  return (
+    <ProtectedLogin>
+      <div className="h-screen flex flex-col bg-slate-100">
+        <Header />
+        <div className="flex-grow flex justify-center items-center">
+          <AnimatePresence mode="wait">
+            <LoginForm />
+          </AnimatePresence>
+        </div>
+      </div>
+    </ProtectedLogin>
+  );
+}
+
+export default Login;
+
