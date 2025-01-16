931d9b76f9b88810f2f4dc45e6c5d1818413f19d
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-16 13:46:59 +0530

    stores different frequencies

---
+++ client/src/constants/serviceFrequency.js
@@ -0,0 +1,12 @@
+const serviceFrequency = {
+    once: "Once",
+    weeklyTwice: "Weekly Twice",
+    daily: "Daily",
+    weekly: "Weekly",
+    fortNightly: "Fortnightly",
+    monthly: "Monthly",
+    quarterly: "Quarterly",
+    halfYearly: "Half Yearly",
+    yearly: "Yearly"
+};
+export default serviceFrequency;
