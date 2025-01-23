73d08fd37234aeaa1b76b4641fe5e13878e57139
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-23 10:48:59 +0530

    pages created for coupons, profile, quotations, review, scheduled services,  service history

---
+++ service/src/pages/Coupons.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Coupons() {
+  return (
+    <div>
+      This is coupons page
+    </div>
+  )
+}
+
+export default Coupons
---
+++ service/src/pages/ServiceHistory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServiceHistory() {
+  return (
+    <div>
+      this is servcie history
+    </div>
+  )
+}
+
+export default ServiceHistory
