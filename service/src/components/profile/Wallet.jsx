86a98e205ec850d2121eb8641c77652e4291a399
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-24 12:18:52 +0530

    change password implemented for service Provider

--- service/src/components/profile/Wallet.jsx
+++ service/src/components/profile/Wallet.jsx
@@ -7,7 +7,7 @@ function Wallet() {
     initial={{ opacity: 0, x: 1000 }}
     animate={{ opacity: 1, x: 0 }}
     exit={{ opacity: 0, x: 1000 }}
-    transition={{ duration: 0.5  }}
+    // transition={{ duration: 0.5  }}
     className="absolute bg-white w-full h-full mx-2"
   >
     This is wallet
