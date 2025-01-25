eac98458bd6546264848673c5f63dc701e171fc7
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-25 17:41:02 +0530

    quotation listing component added

--- client/src/pages/Quotes.jsx
+++ client/src/pages/Quotes.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import ListQuotations from '../components/quotations/ListQuotations'
 function Quotes() {
   return (
-    <div>
-      qhuotes
+    <div className='w-[90vw] md:w-[50vw] mx-auto px-5 py-16 flex-grow'>
+      <ListQuotations />
     </div>
   )
 }
