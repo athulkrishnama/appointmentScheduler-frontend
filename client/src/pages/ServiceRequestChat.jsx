f12541aa8d4cb76a7bf0c4da7b8cb36a0ef09209
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-26 20:55:23 +0530

    page for service requets chat in clientside

---
+++ client/src/pages/ServiceRequestChat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ServiceRequestChatComponent from '../components/quotations/ServiceRequestChat'
+function ServiceRequestChat() {
+  return (
+      <ServiceRequestChatComponent />
+  )
+}
+
+export default ServiceRequestChat
