38aabf861a7d75a6dc26c51bdb7ce32b226cf06f
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-26 20:56:55 +0530

    message component modified to show messages

--- service/src/components/serviceRequests/Message.jsx
+++ service/src/components/serviceRequests/Message.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
-function Message() {
+function Message({ message }) {
   return (
-    <div>
-      this is message
-    </div>
-  )
+    <motion.div
+      className='bg-white shadow-md rounded-lg p-4 mb-4'
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 10 }}
+      transition={{ duration: 0.3 }}
+    >
+      <p className='text-black'>{message.message}</p>
+    </motion.div>
+  );
 }
 
-export default Message
+export default Message;
