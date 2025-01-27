c34e6a680cd92b437e4f9729e761d3d5654aa017
athulkrishnama <athulkrishnama24@gmail.com>
2025-01-27 12:26:34 +0530

    additional info added in service details

--- client/src/components/quotations/ServiceRequestDetails.jsx
+++ client/src/components/quotations/ServiceRequestDetails.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 function ServiceRequestDetails({ serviceRequest }) {
+  const animationVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0 }
+  };
+
   if (!Object.keys(serviceRequest).length) {
     return <div>Loading...</div>;
   }
+
   return (
     <motion.div
       className='bg-white shadow-lg rounded-lg p-6 mb-4'
@@ -16,26 +22,83 @@ function ServiceRequestDetails({ serviceRequest }) {
       <h2 className='text-black text-xl font-bold mb-4'>Service Request Details</h2>
       <table className='w-full border-collapse'>
         <tbody>
-          <tr className='border-b'>
+          <motion.tr 
+            variants={animationVariants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.1 }}
+            className='border-b'
+          >
             <td className='p-2 font-semibold'>Client Name:</td>
             <td className='p-2'>{serviceRequest.client.fullname}</td>
-          </tr>
-          <tr className='border-b'>
+          </motion.tr>
+          <motion.tr 
+            variants={animationVariants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.2 }}
+            className='border-b'
+          >
             <td className='p-2 font-semibold'>Service:</td>
             <td className='p-2'>{serviceRequest.service.serviceName}</td>
-          </tr>
-          <tr className='border-b'>
+          </motion.tr>
+          <motion.tr 
+            variants={animationVariants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.3 }}
+            className='border-b'
+          >
             <td className='p-2 font-semibold'>Date:</td>
             <td className='p-2'>{serviceRequest.date}</td>
-          </tr>
-          <tr className='border-b'>
+          </motion.tr>
+          <motion.tr 
+            variants={animationVariants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.4 }}
+            className='border-b'
+          >
             <td className='p-2 font-semibold'>Time:</td>
             <td className='p-2'>{serviceRequest.time}</td>
-          </tr>
-          <tr className='border-b'>
+          </motion.tr>
+          <motion.tr 
+            variants={animationVariants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.5 }}
+            className='border-b'
+          >
             <td className='p-2 font-semibold'>Notes:</td>
             <td className='p-2'>{serviceRequest.additionalNotes}</td>
-          </tr>
+          </motion.tr>
+          <motion.tr 
+            variants={animationVariants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 0.6 }}
+            className='border-b'
+          >
+            <td className='p-2 font-semibold'>Address:</td>
+            <td className='p-2'>
+              {serviceRequest.address?.fullName}, {serviceRequest.address?.area}, 
+              {serviceRequest.address?.district}, {serviceRequest.address?.state}, 
+              {serviceRequest.address?.pincode}
+            </td>
+          </motion.tr>
+          {serviceRequest.additionalDetails?.map((detail, index) => (
+            <motion.tr 
+              key={index}
+              variants={animationVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{ delay: 0.7 + index * 0.1 }}
+              className='border-b'
+            >
+              <td className='p-2 font-semibold'>{detail.fieldName}:</td>
+              <td className='p-2'>{detail.value}</td>
+            </motion.tr>
+          ))}
         </tbody>
       </table>
     </motion.div>
