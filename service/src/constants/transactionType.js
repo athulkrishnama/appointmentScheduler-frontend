c8765ea9c27ca594ce363d1791080d6a17bb8219
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-05 16:23:41 +0530

    constant file for adding transaction types added

---
+++ service/src/constants/transactionType.js
@@ -0,0 +1,9 @@
+const transactionTypes = {
+    SERVICE_PAYMENT: "service_payment",
+    REFUND: "refund",
+    WITHDRAWAL: "withdrawal",
+    DEPOSIT: "deposit",
+    COMMISSION_DEDUCTION: "commission_deduction"
+}
+
+export default transactionTypes
