3814c84333dbaee4093961ae247313a8a9d307a4
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-08 09:33:19 +0530

    predefined cancellation reasons added

---
+++ service/src/constants/cancellationReason.js
@@ -0,0 +1,14 @@
+const cancellationReasons = [
+  "Scheduling conflict",
+  "Client unresponsive",
+  "Emergency situation",
+  "Staff unavailability",
+  "Technical issues",
+  "Incorrect booking details",
+  "Service area not covered",
+  "Pricing disagreement",
+  "Overbooked slots",
+  "Others",
+];
+
+export default cancellationReasons;
