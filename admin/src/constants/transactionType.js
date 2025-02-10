30cf214256be9bc5dd73524c8623066387e74761
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-10 15:21:53 +0530

    created transactino types in admin app

---
+++ admin/src/constants/transactionType.js
@@ -0,0 +1,10 @@
+const transactionTypes = {
+    SERVICE_PAYMENT: "service_payment",
+    REFUND: "refund",
+    WITHDRAWAL: "withdrawal",
+    DEPOSIT: "deposit",
+    COMMISSION_DEDUCTION: "commission_deduction",
+    PLATFORM_FEE: "platform_fee"
+}
+
+export default transactionTypes
