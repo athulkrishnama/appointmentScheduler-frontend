8fe6b7d04c0b7ba947ac1496421a2be00bf2cfcc
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-22 14:20:33 +0530

    data fetching moved to child component

--- service/src/components/dashboard/Dashboard.jsx
+++ service/src/components/dashboard/Dashboard.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { Chart, registerables } from "chart.js";
 import { Bar } from "react-chartjs-2";
 import { Link } from "react-router";
 import { TbReportAnalytics } from "react-icons/tb";
+import axios from "../../axios/axios";
 
 Chart.register(...registerables);
 
-function Dashboard({charData}) {
+function Dashboard({}) {
+  const [period, setPeriod] = useState('weekly');
+  const [charData, setCharData] = useState([])
+
 //   const chartRef = useRef<Chart | null>(null);
 
   const data = {
@@ -31,13 +35,18 @@ function Dashboard({charData}) {
     
   };
 
-//   useEffect(() => {
-//     return () => {
-//       if (chartRef.current) {
-//         chartRef.current.destroy();
-//       }
-//     };
-//   }, []);
+  const getData = async ()=>{
+    try{
+      const response = await axios.get(`/serviceProvider/dashboard?period=${period}`)
+      console.log(response.data)
+      setCharData(response.data.data)
+    }catch(error){
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    getData()
+  },[period])
 
   return (
     <motion.div
@@ -47,7 +56,14 @@ function Dashboard({charData}) {
       className="mt-10 mx-auto p-4 w-[90vw] md:w-[60vw] h-[80vh] rounded-2xl shadow-2xl"
     >
       <h1 className="text-4xl my-5 font-black text-center text-gray-800">Dashboard</h1>
-      <div className="flex justify-end">
+      <div className="flex justify-between">
+        <div>
+          <select onChange={(e) => setPeriod(e.target.value)} name="" id="" className="my-5 p-2 text-xl flex items-center gap-2 rounded-lg  font-bold">
+            <option value="weekly">Weekly</option>
+            <option value="monthly">Monthly</option>
+            <option value="yearly">Yearly</option>
+          </select>
+        </div>
         <Link to="/salesReport" className="my-5 p-2 text-xl flex items-center gap-2 rounded-lg bg-gray-800 text-white font-bold"><TbReportAnalytics />Sales Report</Link>
       </div>
       <motion.div>
--- service/src/pages/Home.jsx
+++ service/src/pages/Home.jsx
@@ -1,24 +1,11 @@
 import React,{useEffect, useState} from 'react'
 import Dashboard from '../components/dashboard/Dashboard'
-import axios from '../axios/axios'
 function Home() {
-  const [data, setData] = useState([])
 
-  const getData = async ()=>{
-    try{
-      const response = await axios.get('/serviceProvider/dashboard')
-      console.log(response.data)
-      setData(response.data)
-    }catch(error){
-      console.log(error)
-    }
-  }
-  useEffect(()=>{
-    getData()
-  },[])
+
   return (
     <>
-      <Dashboard charData={data} />
+      <Dashboard />
     </>
   )
 }
