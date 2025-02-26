76eb80845e2cb47fa76973ba22bf882138d40f7f
athulkrishnama <athulkrishnama24@gmail.com>
2025-02-26 14:41:38 +0530

    constant file for report reasons

---
+++ client/src/constants/reportReasons.js
@@ -0,0 +1,13 @@
+const reportReasons = {
+    bad_behaviour: "Bad behavior of the service provider",
+    poor_service_quality: "Poor quality of service",
+    overcharging: "Overcharging or unexpected additional fees",
+    no_show: "Service provider did not show up",
+    false_advertising: "Service was not as advertised",
+    unprofessional_conduct: "Unprofessional conduct or communication",
+    safety_concerns: "Safety concerns during the service",
+    fraud_scam: "Fraudulent activity or scam",
+    late_arrival: "Service provider arrived significantly late",
+    other:"other"
+};
+export default reportReasons;
